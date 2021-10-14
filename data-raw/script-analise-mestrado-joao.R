36055e30bfe96d3ff3aaefe44c5e53baa5db8a49
Alan R Panosso <arpanosso@yahoo.com.br>
2021-10-14 15:26:57 -0300

    ADIÇÃO DO README

--- data-raw/script-analise-mestrado-joao.R
+++ data-raw/script-analise-mestrado-joao.R
@@ -1,10 +1,24 @@
 library(tidyverse)
+library(lubridate)
 library(skimr)
 library(GGally)
 
 dados<- readxl::read_excel("data-raw/Dados Mestrado João.xlsx") |>
   janitor::clean_names()
 dplyr::glimpse(dados)
+dados <- dados |>
+  select(data,
+         repeticao,
+         linhagem,
+         nivel_de_fosforo_disp,
+         resistencia_kgf,
+         frequencia_ressonante,
+         shape_index,
+         peso_g,
+         altura_de_albumen,
+         uh,
+         espessura_mm
+         )
 
 dados <-dados |>
   dplyr::mutate(
@@ -22,14 +36,35 @@ dados <-dados |>
 dplyr::glimpse(dados)
 
 dados |>
-  dplyr::group_by(dia) |>
-  dplyr::summarise(re=mean(resistencia_kgf))
+  dplyr::group_by(dia, linhagem) |>
+  dplyr::summarise(re=mean(resistencia_kgf, na.rm=TRUE)) |>
+  ggplot(aes(x=dia, y=re, color=linhagem)) +
+  geom_point() +
+  geom_smooth(method = "lm")
+
+tab <- dados |>
+  dplyr::group_by(dia,linhagem) |>
+  dplyr::summarise(re=mean(resistencia_kgf, na.rm=TRUE))
+
+mod <- lm(re~dia + linhagem, data=tab)
+summary.lm(mod)
+plot(mod)
 
 skim(dados)
 
 dados |>
-  ggplot(aes(x = as.factor(tratamento), y = resistencia_kgf)) +
-  geom_boxplot()
+  ggplot(aes(x = as.factor(nivel_de_fosforo_disp), y = resistencia_kgf, fill=as.factor(dia))) +
+  geom_boxplot() +
+  facet_wrap(~dia)
+
+dados |>
+  group_by(nivel_de_fosforo_disp, linhagem, repeticao) |>
+  summarise(re = mean(resistencia_kgf, na.rm=TRUE)) |>
+  ggplot(aes(x = as.factor(nivel_de_fosforo_disp), y = re)) +
+  geom_boxplot(fill="lightgray") +
+  theme_bw() +
+  labs(x="Nível de Fósforo disponível (%)",
+       y = "Resistência (kgf)")
 
 dados |>
   dplyr::filter(! (frequencia_ressonante < 4500 | frequencia_ressonante >6000)) |>
@@ -39,24 +74,33 @@ dados |>
   geom_point() +
   geom_smooth(method = "lm")
 
-visdat::vis_miss(dados)
 
-
+names(dados)
 dados  |>
-  #filter(dia ==60) |>
-  select(frequencia_ressonante:percent_casca)  |>
+  filter(linhagem == "Dekalb") |>
+  select(resistencia_kgf:espessura_mm)  |>
   cor(use = "p")  |>
   corrplot::corrplot()
 
-glimpse(dados)
+dados  |>
+  filter(linhagem == "H&N") |>
+  select(resistencia_kgf:espessura_mm)  |>
+  cor(use = "p")  |>
+  corrplot::corrplot()
 
 dados |>
-  #filter(dia ==15) |>
-  select(frequencia_ressonante:percent_casca) |>
+  filter(linhagem == "Dekalb") |>
+  select(resistencia_kgf:espessura_mm) |>
   ggpairs()
 
 dados |>
-  ggplot(aes(x=frequencia_ressonante)) +
-  geom_histogram(bins=30,color="black",fill="lightgray")
+  filter(linhagem == "H&N") |>
+  select(resistencia_kgf:espessura_mm) |>
+  ggpairs()
+
+dados |>
+  ggplot(aes(x=frequencia_ressonante, fill=linhagem)) +
+  geom_histogram(bins=30,color="black") +
+  facet_wrap(~linhagem)
 
 
